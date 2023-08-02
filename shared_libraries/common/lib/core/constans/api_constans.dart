c6b80103992238d7eaeda360ab312adbbecc99ee
Bintaaaa <bijantium.yt.32@gmail.com>
2023-08-03 00:39:21 +0700

    Update api_constans.dart

--- shared_libraries/common/lib/core/constans/api_constans.dart
+++ shared_libraries/common/lib/core/constans/api_constans.dart
@@ -4,7 +4,7 @@ class ApiConstans {
   String get baseUrl => "https://pokeapi.co/api/v2/";
 
   //[INFO]  Get all about pokemons
-  String get pokemons => "pokemons";
+  String get pokemons => "pokemon";
 
   String get baseImageUrl =>
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
